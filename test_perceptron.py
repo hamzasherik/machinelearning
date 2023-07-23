9a3bce898c83b46074993f7a8b6fc659372791ae
Hamza Sherik <45083184+hamzasherik@users.noreply.github.com>
2023-07-23 15:42:59 -0400

    moving test_perceptron to unit test directory

---
+++ test_perceptron.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+import numpy as np
+import pandas as pd
+from perceptron import Perceptron
+
+data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+
+class TestPerceptrons(TestCase):
+
+    def setUp(self):
+        data = pd.read_csv(data_url).iloc[:99].values
+
+        self.X = data[:,:4]
+        self.y = data[:, 4]
+
+        # first 49 elements of dataset are setosa, the next 50 are versicolor
+        self.y_preprocessed_arr = np.concatenate((np.zeros(49, dtype=float), np.ones(50, dtype=float)))
+
+    def test_init(self):
+        perceptron_instance = Perceptron(eta=0.1, epochs=10)
+
+        self.assertEqual(perceptron_instance.eta, 0.1)
+        self.assertEqual(perceptron_instance.epochs, 10)
+        self.assertEqual(perceptron_instance.random_state, 1)
+        self.assertEqual(perceptron_instance.threshold_accuracy, 0.8)
+
+    def test_preprocess(self):
+        perceptron_instance = Perceptron(eta=0.1, epochs=10)
+
+        X_preprocessed, y_preprocessed = perceptron_instance.preprocess(self.X, self.y)
+
+        np.testing.assert_array_almost_equal(X_preprocessed, self.X)
+        np.testing.assert_array_almost_equal(y_preprocessed, self.y_preprocessed_arr)
+
+    def test_lifecycle(self):
+        perceptron_instance = Perceptron(eta=0.1, epochs=10)
+
+        accuracy = perceptron_instance.lifecycle(self.X, self.y)
+
+        self.assertTrue(accuracy)
+        self.assertAlmostEqual(1.0, perceptron_instance.accuracy_)
+
