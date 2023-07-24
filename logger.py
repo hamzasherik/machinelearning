cb8afd109f170c0096bc688ee97171bfffd08bfe
Hamza Sherik <45083184+hamzasherik@users.noreply.github.com>
2023-07-24 13:55:41 -0400

    moved logger to utils folder

--- logger.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
-logger.handlers[0].setFormatter(logging.Formatter('%(levelname)s: %(funcName)s [Line: %(lineno)d]: %(message)s'))
